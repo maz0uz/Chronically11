8129f394b8afa2b5a1c63a8ae6323ee3e334fe74
maz0uz <124688655+maz0uz@users.noreply.github.com>
2024-11-14 21:08:29 +0200

    Update and rename index.js to dbCommunicator.js

--- Backend/dbCommunicator.js
+++ Backend/dbCommunicator.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const db = require('./database');
+const db = require('./database.js');
 
 const app = express();
 const PORT = 5000;
 
-app.get('/data', (req, res) => {
-    db.query('SELECT * FROM Tweets', (err, results) => {
+app.get('/tweets/:Category', (req, res) => {
+    const tweetCategory = req.params.Category;
+
+    db.query('SELECT * FROM Tweets WHERE categories = ?', [tweetCategory], (err, results) => {
         if (err) {
             res.status(500).json({ error: err });
         } else {
-            res.json([results]);
+            res.json(results);
         }
     });
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

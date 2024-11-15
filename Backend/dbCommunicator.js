c3adb294e0944f5ff7c7db17d6523cf5a8b915de
maz0uz <124688655+maz0uz@users.noreply.github.com>
2024-11-15 14:09:40 +0200

    Update dbCommunicator.js

--- Backend/dbCommunicator.js
+++ Backend/dbCommunicator.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const db = require('./database.js');
+const mysql = require('mysql');
 
 const app = express();
 const PORT = 5000;
 
-app.get('/tweets/:Category', (req, res) => {
-    const tweetCategory = req.params.Category;
+const connection = mysql.createConnection({
+    host: 'chronicallyswe.c1ikges4mouc.eu-north-1.rds.amazonaws.com',
+    user: 'root',
+    password: 'swedatabase',
+    database: 'chronically'
+});
 
-    db.query('SELECT * FROM Tweets WHERE categories = ?', [tweetCategory], (err, results) => {
-        if (err) {
-            res.status(500).json({ error: err });
-        } else {
-            res.json(results);
-        }
-    });
+connection.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err.stack);
+        return;
+    }
+    console.log('Connected to database.');
+});
+
+app.get('/tweets', (req, res) => {
+    const sqlQuery = "SELECT * FROM Tweets"
+
+    connection.query(sqlQuery, (err,data) => {
+        if (err)  return res.json(err);
+        return res.json(data);
+        })
 });
 
 
